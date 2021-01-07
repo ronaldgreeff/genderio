9e01f5f44b0a5ac8693636c592e42d2e521a802d
ronaldgreeff <ronald.greeff@gmail.com>
2021-01-07 13:15:59 +0000

    using wtf-forms

--- project/auth.py
+++ project/auth.py
@@ -1,60 +1,123 @@
 from flask import (Blueprint, render_template, redirect, url_for, request,
     flash)
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user
+# from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_required, logout_user, current_user, login_user
 from .models import User
 from . import db
-
+from .forms import SigninForm, SignupForm
 
 auth = Blueprint('auth', __name__)
 
-
-@auth.route('/signin')
+@auth.route('/signin', methods=['GET', 'POST'])
 def signin():
-    return render_template("signin.html")
+    """
+    Log-in page for registered users.
 
-@auth.route('/signin', methods=['POST'])
-def signin_post():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    remember = True if request.form.get('remember') else False
+    GET requests serve Log-in page.
+    POST requests validate and redirect user to dashboard.
+    """
+    # Bypass if user is logged in
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
 
-    user = User.query.filter_by(email=email).first()
-
-    if not user or not check_password_hash(user.password, password):
-        flash("Please check your login details and try again.")
+    form = SigninForm()
+    # Validate login attempt
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.check_password(password=form.password.data):
+            login_user(user)
+            next_page = request.args.get('next')
+            return redirect(next_page or url_for('main.index'))
+        flash('Invalid username/password combination')
         return redirect(url_for('auth.signin'))
+    return render_template(
+        'signin.jinja2',
+        form=form,
+        title='Log in.',
+        template='login-page',
+        body="Log in with your User account."
+    )
 
-    login_user(user, remember=remember)
-    return redirect(url_for('main.index'))
-
-
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
+    """
+    User sign-up page.
 
-@auth.route('/signup', methods=['POST'])
-def signup_post():
-    email = request.form.get('email')
-    name = request.form.get('name')
-    password = request.form.get('password')
-    repeat_password = request.form.get('repeat_password')
+    GET requests serve sign-up page.
+    POST requests validate form & user creation.
+    """
+    form = SignupForm()
+    if form.validate_on_submit():
+        existing_user = User.query.filter_by(email=form.email.data).first()
+        if existing_user is None:
+            user = User(
+                name=form.name.data,
+                email=form.email.data,
+            )
+            user.set_password(form.password.data)
+            db.session.add(user)
+            db.session.commit()  # Create new user
+            login_user(user)  # Log in as newly created user
+            return redirect(url_for('main.index'))
+        flash('A user already exists with that email address.')
+    return render_template(
+        'signup.jinja2',
+        title='Create an Account.',
+        form=form,
+        template='signup-page',
+        body="Sign up for a user account."
+    )
 
-    if password != repeat_password:
-        flash("Passwords don't match.")
-        return redirect(url_for('auth.signup'))
 
-    user = User.query.filter_by(email=email).first()
-    if user:
-        flash("Email address already exists.")
-        return redirect(url_for('auth.signup'))
-
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
-
-    db.session.add(new_user)
-    db.session.commit()
-
-    return redirect(url_for('auth.signin'))
+# @auth.route('/signin')
+# def signin():
+#     return render_template("signin.html")
+#
+# @auth.route('/signin', methods=['POST'])
+# def signin_post():
+#     email = request.form.get('email')
+#     password = request.form.get('password')
+#     remember = True if request.form.get('remember') else False
+#
+#     user = User.query.filter_by(email=email).first()
+#
+#     # if not user or not check_password_hash(user.password, password):
+#     #     flash("Please check your login details and try again.")
+#     #     return redirect(url_for('auth.signin'))
+#     # TODO:
+#
+#     login_user(user, remember=remember)
+#     return redirect(url_for('main.index'))
+#
+#
+# @auth.route('/signup')
+# def signup():
+#     return render_template("signup.html")
+#
+# @auth.route('/signup', methods=['POST'])
+# def signup_post():
+#     email = request.form.get('email')
+#     name = request.form.get('name')
+#     password = request.form.get('password')
+#     repeat_password = request.form.get('repeat_password')
+#
+#     if password != repeat_password:
+#         flash("Passwords don't match.")
+#         return redirect(url_for('auth.signup'))
+#
+#     user = User.query.filter_by(email=email).first()
+#     if user:
+#         flash("Email address already exists.")
+#         return redirect(url_for('auth.signup'))
+#
+#     # new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+#     new_user = User(email=email, name=name)
+#     new_user.set_password()
+#
+#     db.session.add(new_user)
+#     db.session.commit()
+#
+#     return redirect(url_for('auth.signin'))
 
 
 @auth.route('/logout')
--- project/models.py
+++ project/models.py
@@ -1,8 +1,111 @@
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 
+
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
+    """User account model."""
+
+    __tablename__ = 'user'
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    name = db.Column(
+        db.String(100),
+        nullable=False,
+        unique=False
+    )
+    email = db.Column(
+        db.String(40),
+        unique=True,
+        nullable=False
+    )
+    password = db.Column(
+        db.String(200),
+        primary_key=False,
+        unique=False,
+        nullable=False
+	)
+    created_on = db.Column(
+        db.DateTime,
+        index=False,
+        unique=False,
+        nullable=True
+    )
+    last_login = db.Column(
+        db.DateTime,
+        index=False,
+        unique=False,
+        nullable=True
+    )
+    babies = db.relationship('Baby', backref='user', lazy=True,)
+
+    def set_password(self, password):
+        """Create hashed password."""
+        self.password = generate_password_hash(
+            password,
+            method='sha256'
+        )
+
+    def check_password(self, password):
+        """Check hashed password."""
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+class Baby(db.Model):
+    """Baby model"""
+
+    __tablename__ = 'baby'
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    name = db.Column(
+        db.String(100),
+        nullable=False,
+        unique=False
+    )
+    dob = db.Column(
+        db.DateTime,
+        index=False,
+        unique=False,
+        nullable=True
+    )
+    parent_id = db.Column(db.Integer, db.ForeignKey('user.id'),)
+    images = db.relationship('BabyImg', backref='baby', lazy=True,)
+
+    def __repr__(self):
+        return '<Baby {}>'.format(self.name)
+
+
+class BabyImg(db.Model):
+    """Baby picture model"""
+
+    __tablename__ = 'babyimg'
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    weeks = db.Column(
+        db.Integer,
+        unique=False,
+        nullable=False,
+    )
+    days = db.Column(
+        db.Integer,
+        unique=False,
+        nullable=False,
+    )
+    filename = db.Column(
+        db.String(100),
+        nullable=False,
+        unique=False
+    )
+    baby_id = db.Column(db.Integer, db.ForeignKey('baby.id'))
+
+    def __repr__(self):
+        return '<Img {}>'.format(self.filename)
