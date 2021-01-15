fc045e9be2fe8e58ea7d08f9c24c833b42bc860c
ronaldgreeff <ronald.greeff@gmail.com>
2021-01-15 18:21:13 +0000

    prediction view stuck @ filepath

--- genderio/app/prediction/views.py
+++ genderio/app/prediction/views.py
@@ -1,4 +1,6 @@
 import os
+import pathlib
+
 from ..helpers.utils import get_img_filename, save_image, dtdob
 from flask import Flask, request, send_from_directory, redirect, url_for
 from flask import jsonify
@@ -11,21 +13,28 @@ from sqlalchemy.orm import joinedload
 from .. import db
 from ..models import User, Baby, BabyImg
 
+import keras
+from keras.models import load_model
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+
 from . import prediction
 
 # keras disabled for now
 # from .helpers_keras import fetch_model
 # model = fetch_model()
 
-# model = load_model(pathlib.Path('models/tl2_final'))
-# model.load_weights(pathlib.Path('models/tl2_final.h5'))
+model = load_model(os.path.join(os.getcwd(), 'app/prediction/models/tl2_final'))
+model.load_weights(os.path.join(os.getcwd(), 'app/prediction/models/tl2_final.h5'))
 
 def predict_gender(baby):
     d = {'male': [], 'female': []}
     baby_image_filepaths = db.session.query(BabyImg.filepath).filter(BabyImg.baby_id==baby.id).all()
     for baby_image in baby_image_filepaths:
-        preds = model.predict(baby_image)
-        print("preds", preds)
+        print(baby_image)
+
+    # preds = model.predict(baby_image)
+    # print("preds", preds)
 
     # image_paths = [
     #     'boy/286242_5d3ca9f6-8d8a-408c-bade-01a6efddca6d.jpg',
@@ -33,21 +42,22 @@ def predict_gender(baby):
     # ]
     #
     # preds = []
-    #
+
     # for image_path in image_paths:
-    #     image = keras.preprocessing.image.load_img(
-    #         pathlib.Path('data/{}'.format(image_path)),
-    #         color_mode="grayscale",
-    #         target_size=(128, 128),
-    #     )
-    #     input_array = keras.preprocessing.image.img_to_array(image)
-    #     input_array = np.array([input_array])
-    #     pred = model.predict(input_array)
-    #
-    #     cl = np.round(pred)
-    #     pr = pred[:,0]
-    #
-    #     print(pr, cl)
+
+        image = keras.preprocessing.image.load_img(
+            '../{}'.format(baby_image[0]),
+            color_mode="grayscale",
+            target_size=(128, 128),
+        )
+        input_array = keras.preprocessing.image.img_to_array(image)
+        input_array = np.array([input_array])
+        pred = model.predict(input_array)
+
+        cl = np.round(pred)
+        pr = pred[:,0]
+
+        print(pr, cl)
 
     # query babies images
     # for images in images:
